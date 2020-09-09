096437c80c45e6e516ca187e88b6a73a57486e74
Mike Siddall <eatingartist@gmail.com>
2020-09-09 16:01:02 -0500

    client side delete button working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,48 +1,72 @@
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
-function onReady(){
-    getSongs();
-    $( '#addSongButton' ).on( 'click', addSong );
+function onReady() {
+  getSongs();
+  $("#addSongButton").on("click", addSong);
+  $(document).on("click", ".deleteSongBtn", deleteSong);
 } // end onReady
 
-function addSong(){
-    let objectToSend = {
-        rank: $( '#rankIn' ).val(),
-        artist: $( '#artistIn' ).val(),
-        track: $( '#trackIn' ).val(),
-        published: $( '#publishedIn' ).val()
-    } // end object to send
-    $.ajax({
-        method: 'POST',
-        url: '/songs',
-        data: objectToSend
-    }).then( function( response ){
-        console.log( 'back from POST with:', response );
-    }).catch( function( err ){
-        alert( 'error!' );
-        console.log( err );
-    }) // end AJAX POST
+function deleteSong() {
+  // Grab the `data-id` attribute
+  // from our "Button"
+  let songId = $(this).data("id"); // ToDo
+  $.ajax({
+    method: "DELETE",
+    url: `/songs/${songID}`,
+  })
+    .then(function (response) {
+      console.log("Deleted!", response);
+    })
+    .catch(function (err) {
+      console.log("Error in delete", err);
+      alert("ruh-roh");
+    });
+}
+function addSong() {
+  let objectToSend = {
+    rank: $("#rankIn").val(),
+    artist: $("#artistIn").val(),
+    track: $("#trackIn").val(),
+    published: $("#publishedIn").val(),
+  }; // end object to send
+  $.ajax({
+    method: "POST",
+    url: "/songs",
+    data: objectToSend,
+  })
+    .then(function (response) {
+      console.log("back from POST with:", response);
+    })
+    .catch(function (err) {
+      alert("error!");
+      console.log(err);
+    }); // end AJAX POST
 } // end addSong
 
-function getSongs(){
-    $.ajax({
-        method: 'GET',
-        url: '/songs'
-    }).then( function( response ){
-        console.log( 'back from GET with:', response ); 
-        // display songs on DOM 
-        let el = $( '#songsOut' );
-        el.empty();
-        for( let i=0; i<response.length; i++ ){
-            el.append( `<li>
-            ${ response[i].rank }
-            ${ response[i].track }
-            ${ response[i].artist }
-            ${ response[i].published.split( 'T' )[0] }
-            </li>`)
-        } // end for
-    }).catch( function( err ){
-        alert( 'error!' );
-        console.log( err );
-    }) // end AJAX GET
+function getSongs() {
+  $.ajax({
+    method: "GET",
+    url: "/songs",
+  })
+    .then(function (response) {
+      console.log("back from GET with:", response);
+      // display songs on DOM
+      let el = $("#songsOut");
+      el.empty();
+      for (let i = 0; i < response.length; i++) {
+        el.append(`<li>
+            ${response[i].rank}
+            ${response[i].track}
+            ${response[i].artist}
+            ${response[i].published.split("T")[0]}
+            <button class="deleteSongBtn" data-id="${
+              response[i].id
+            }">Delete Me</button>
+            </li>`);
+      } // end for
+    })
+    .catch(function (err) {
+      alert("error!");
+      console.log(err);
+    }); // end AJAX GET
 } // end getSongs()
